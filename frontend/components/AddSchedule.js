37356b1f424b039fd8b59fad54277e1fa1cfcb39
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2021-09-22 00:17:13 -0400

    Added native-react-paper-datepicker and component

--- frontend/components/AddSchedule.js
+++ frontend/components/AddSchedule.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { View, StyleSheet } from 'react-native';
 import { TextInput, Button, RadioButton, Text } from 'react-native-paper';
+import { TimePickerModal } from 'react-native-paper-dates';
 
 const DaysRadioButton = (props) => {
     const [isSelected, setSelected] = useState(false)
@@ -17,6 +18,19 @@ const DaysRadioButton = (props) => {
 }
 
 const AddSchedule = () => {
+    const [visible, setVisible] = React.useState(false)
+    const onDismiss = React.useCallback(() => {
+      setVisible(false)
+    }, [setVisible])
+  
+    const onConfirm = React.useCallback(
+      ({ hours, minutes }) => {
+        setVisible(false);
+        console.log({ hours, minutes });
+      },
+      [setVisible]
+    );
+
     return (
         <View>
             <TextInput 
@@ -31,6 +45,23 @@ const AddSchedule = () => {
                 <DaysRadioButton day="Thu"/>
                 <DaysRadioButton day="Fri"/>
                 <DaysRadioButton day="Sat"/>
+            </View>
+            <View>
+                <TimePickerModal
+                    visible={visible}
+                    onDismiss={onDismiss}
+                    onConfirm={onConfirm}
+                    hours={12} // default: current hours
+                    minutes={14} // default: current minutes
+                    label="Select time" // optional, default 'Select time'
+                    cancelLabel="Cancel" // optional, default: 'Cancel'
+                    confirmLabel="Ok" // optional, default: 'Ok'
+                    animationType="fade" // optional, default is 'none'
+                    locale={'en'} // optional, default is automically detected by your system
+                />
+                <Button onPress={()=> setVisible(true)}>
+                    Pick time
+                </Button>
             </View>
             <View style={styles.buttons}>
                 <Button icon="check" mode="contained">Submit</Button>
