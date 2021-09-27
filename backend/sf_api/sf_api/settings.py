b2bb2e39314732865c746cd9c7fef894fed9a686
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2021-09-27 16:01:45 -0400

    Modified settings.py

--- backend/sf_api/sf_api/settings.py
+++ backend/sf_api/sf_api/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'sf_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'helloworld',
+        # 'USER': '<yourname>',
+        # 'PASSWORD': 'password',
+        # 'HOST': 'localhost',
+        # 'PORT': '',
     }
 }
 
