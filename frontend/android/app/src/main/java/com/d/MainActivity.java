2770d1bc4807208da281e89564c4d62f9044ae04
Alyssa <Alyssa.Ma80@myhunter.cuny.edu>
2021-09-29 15:54:34 -0400

    redid setup for react nav

--- frontend/android/app/src/main/java/com/d/MainActivity.java
+++ frontend/android/app/src/main/java/com/d/MainActivity.java
@@ -1,5 +1,6 @@
 package com.d;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +12,9 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "d";
+  }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
