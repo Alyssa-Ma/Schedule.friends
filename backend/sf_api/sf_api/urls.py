b71ba69e6c4860e32e2a277c087e24e56c828f81
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2021-10-01 01:04:17 -0400

    Added views

--- backend/sf_api/sf_api/urls.py
+++ backend/sf_api/sf_api/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from sf_users import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^api/sf_users/$', views.users_list),
+    re_path(r'^api/sf_users/([0-9])$', views.users_detail)
 ]
--- backend/sf_api/sf_users/serializers.py
+++ backend/sf_api/sf_users/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import User
 
-class userSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
