0750d6a06a6348f5e7744e3ccdc2f5d82d2b4438
Henry-Cevallos <henry.cevallos95@myhunter.cuny.edu>
2021-10-12 15:51:43 -0400

    added calls in addFriend

--- frontend/components/AddFriend.js
+++ frontend/components/AddFriend.js
@@ -1,64 +1,83 @@
 import React, {useState} from 'react';
 import { View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import { Button, Paragraph, Dialog, Portal, Provider } from 'react-native-paper';
 
 //Header function. Uses the prop title that has been passed
 const AddFriend = ({title}) => {
 
     const [text, setText] = useState('');
+    const [visible, setVisible] = useState(false);
+
+    const showDialog = () => setVisible(true);
+    const hideDialog = () => setVisible(false);
 
     const onChange = textValue => {
-
-        //console.log(textValue);
         setText(textValue);
     }
+
     const onPressBtn = async () => {
         
-        //manipulate data here
-        const data = {
-            "from_user": 1,
-            "to_user": 3
-        };
+        showDialog();
 
-        //actual POST
-        /*
-            //swap url with actual server when deployed
-            fetch('http://127.0.0.1:8000/api/sf_users/friend_requests/', {
-                method: 'POST', // or 'PUT'
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data),
-              }).then( response => response.json())
-              .then( data => {
-                  console.log(data);
-              })
-              .catch( error => {
-                  console.log(error);
-              })
-        */
-        
+        //GET friend info based on user input
+        let friend_json;
         try {
-            const resonse = await fetch('http://10.0.2.2:8000/api/sf_users/');
-            console.log('why');
-            const json = await resonse.json();
-            console.log(json);
+            const resonse = await fetch(`http://10.0.2.2:8000/api/sf_users/?query=${text}`);
+            friend_json = await resonse.json();
+            
         } catch(error) {
             console.log(error);
         }
-        
+        const friend_id = friend_json[0].id;
 
-        
+        const data = {
+            from_user: 1,
+            to_user: friend_id
+        }
+
+        //actual POST. Create the friend request
+        //swap url with actual server when deployed
+        fetch('http://10.0.2.2:8000/api/sf_users/friend_requests/', {
+            method: 'POST', // or 'PUT'
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        }).then( response => response.json())
+        .then( data => {
+            console.log(data);
+        })
+        .catch( error => {
+            console.log(error);
+        })
+         
     }
    
     return (
-        <View style={styles.container}>
-            <TextInput placeholder="Add Email" style={styles.input} onChangeText={onChange}/>
 
+        <Provider>
+        <View style={styles.container}>
+            <TextInput placeholder="Enter Username" style={styles.input} onChangeText={onChange}/>
+            
+            <Portal>
+                <Dialog visible={visible} onDismiss={hideDialog}>
+                    <Dialog.Title>Friend Request Sent!</Dialog.Title>
+                    <Dialog.Content>
+                        <Paragraph>Friend request sent to {text}! They will see it in their friend requests!</Paragraph>
+                    </Dialog.Content>
+                    <Dialog.Actions>
+                        <Button onPress={hideDialog}>Done</Button>
+                    </Dialog.Actions>
+                </Dialog>
+            </Portal>
+            
             <TouchableOpacity style={styles.btn} onPress={onPressBtn}>
                 <Text style={styles.btnText}> Send Request</Text>
             </TouchableOpacity>
 
         </View>
+
+        </Provider>
     )
 };
 
