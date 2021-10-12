3e2a46d124d79e2f5fefca16be809fd5a976b455
Alyssa <Alyssa.Ma80@myhunter.cuny.edu>
2021-10-11 22:45:33 -0400

    testing react-native-schedule

--- frontend/screens/CombinedScheduleView.js
+++ frontend/screens/CombinedScheduleView.js
@@ -1,6 +1,36 @@
 import React, {useState} from 'react';
-import { View, Text, StyleSheet, FlatList, SafeAreaView, Button} from 'react-native';
-import Header from '../components/Header';
+import { View, Text, StyleSheet, FlatList, SafeAreaView, Button, Dimensions } from 'react-native';
+import EventCalendar from 'react-native-events-calendar';
+let { width } = Dimensions.get('window');
+
+const events = [
+    { start: '2017-09-07 00:30:00', end: '2017-09-07 01:30:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
+    { start: '2017-09-07 01:30:00', end: '2017-09-07 02:20:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
+    { start: '2017-09-07 04:10:00', end: '2017-09-07 04:40:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
+    { start: '2017-09-07 01:05:00', end: '2017-09-07 01:45:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
+    { start: '2017-09-07 14:30:00', end: '2017-09-07 16:30:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
+    { start: '2017-09-08 01:20:00', end: '2017-09-08 02:20:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
+    { start: '2017-09-08 04:10:00', end: '2017-09-08 04:40:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
+    { start: '2017-09-08 00:45:00', end: '2017-09-08 01:45:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
+    { start: '2017-09-08 11:30:00', end: '2017-09-08 12:30:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
+    { start: '2017-09-09 01:30:00', end: '2017-09-09 02:00:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
+    { start: '2017-09-09 03:10:00', end: '2017-09-09 03:40:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
+    { start: '2017-09-09 00:10:00', end: '2017-09-09 01:45:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' }
+]
+
+
+const CombinedScheduleView = ({ navigation }) => {
+  return (
+    <EventCalendar
+      eventTapped={this.eventTapped.bind(this)}
+      events={this.state.events}
+      width={width}
+      initDate={'2017-09-08'}
+    />
+  );
+}
+
+export default CombinedScheduleView;
 
   /** 
     const data = [
@@ -19,7 +49,7 @@ import Header from '../components/Header';
     const renderItem = ({ item }) => (
         <Item title={item.title} />
       );
-      */
+      
     //whichever screen has the navigation button for stack, must have naviation as a param in their function
   const CombinedScheduleView = ({ navigation }) => {
       return (
@@ -28,13 +58,14 @@ import Header from '../components/Header';
         </View>
       );
     }
-
+*/
 //Style Sheet
 const styles = StyleSheet.create({
 
     container: {
         flex: 1,
-        
+        backgroundColor: '#ffb3bb', 
+        marginTop: 100,
       },
       item: {
         backgroundColor: '#f9c2ff',
@@ -47,5 +78,3 @@ const styles = StyleSheet.create({
       },
 
 });
-
-export default CombinedScheduleView;
