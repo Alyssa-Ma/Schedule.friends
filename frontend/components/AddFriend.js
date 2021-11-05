0cb3eca635fc915c1d52678b43da77039bbb3f5e
kobedejesus87 <kobe.dejesus87@myhunter.cuny.edu>
2021-11-05 13:30:54 -0400

    error handling for SendFriendRequest

--- frontend/components/AddFriend.js
+++ frontend/components/AddFriend.js
@@ -1,5 +1,5 @@
 import React, {useState, useContext} from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Alert} from 'react-native';
 import { Button, Paragraph, Dialog, Portal, Provider } from 'react-native-paper';
 import {BASE_URL} from "@env";
 import UserContext from '../context/UserContext';
@@ -17,9 +17,21 @@ const AddFriend = ({title, route}) => {
         setText(textValue);
     }
 
+    const validation = () => {
+        if((text == "")||(text == " "))
+        {
+            Alert.alert("please enter a username");
+        }
+        else
+        {
+            onPressBtn();
+        }
+    }
+
     const onPressBtn = async () => {
         
         showDialog();
+        console.log("**** onPressBtn is running... ****");
 
         //GET friend info based on user input
         let friend_json;
@@ -37,40 +49,46 @@ const AddFriend = ({title, route}) => {
             console.log(error);
         }
 
-        const friend_id = friend_json[0].id;
-
-        const data = {
-            from_user: context.user.id,   //CURRENT USER
-            to_user: friend_id
+        console.log(friend_json);
+        if(!friend_json.length)
+        {
+            console.log("user not found");
+            Alert.alert("user not found");
         }
+        else
+        {
+            console.log("user found");
+            Alert.alert("Friend Request has been sent or pending");
+            const friend_id = friend_json[0].id;
 
-        //actual POST. Create the friend request
-        //swap url with actual server when deployed
-        fetch(`${BASE_URL}/friend_requests/`, {
-            method: 'POST', // or 'PUT'
-            headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Token ${context.user.token}`
-            },
-            body: JSON.stringify(data),
-        }).then( response => response.json())
-        .then( data => {
-            console.log(data);
-        })
-        .catch( error => {
-            console.log(error);
-        })
-         
+            const data = {
+                from_user: context.user.id,   //CURRENT USER
+                to_user: friend_id
+            }
+
+            //actual POST. Create the friend request
+            //swap url with actual server when deployed
+            fetch(`${BASE_URL}/friend_requests/`, {
+                method: 'POST', // or 'PUT'
+                headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${context.user.token}`
+                },
+                body: JSON.stringify(data),
+            }).then( response => response.json())
+            .then( data => {
+                console.log(data);
+            })
+            .catch( error => {
+                console.log(error);
+            })
+        }
     }
    
-    return (
-
-        <Provider>
-        <View style={styles.container}>
-            <TextInput placeholder="Enter Username" style={styles.input} onChangeText={onChange}/>
-            
+/*
             <Portal>
                 <Dialog visible={visible} onDismiss={hideDialog}>
+                    
                     <Dialog.Title>Friend Request Sent!</Dialog.Title>
                     <Dialog.Content>
                         <Paragraph>Friend request sent to {text}! They will see it in their friend requests!</Paragraph>
@@ -80,8 +98,16 @@ const AddFriend = ({title, route}) => {
                     </Dialog.Actions>
                 </Dialog>
             </Portal>
+*/
+
+
+    return (
+
+        <Provider>
+        <View style={styles.container}>
+            <TextInput placeholder="Enter Username" style={styles.input} onChangeText={onChange}/>
             
-            <TouchableOpacity style={styles.btn} onPress={onPressBtn}>
+            <TouchableOpacity style={styles.btn} onPress={validation}>
                 <Text style={styles.btnText}> Send Request</Text>
             </TouchableOpacity>
 
