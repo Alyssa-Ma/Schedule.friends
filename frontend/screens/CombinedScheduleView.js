0a5232e2abada46dba75564650d76266e6312535
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2021-11-07 23:49:04 -0500

    Schedule will creat dynamic start and end views based on the event times

--- frontend/screens/CombinedScheduleView.js
+++ frontend/screens/CombinedScheduleView.js
@@ -5,30 +5,34 @@ let { width } = Dimensions.get('window');
 import UserContext from '../context/UserContext';
 import LoadingIndicator from '../components/LoadingIndicator';
 import {BASE_URL} from "@env";
-import { useFocusEffect } from '@react-navigation/core';
 
 const CombinedScheduleView = ({navigation, route}) => {
-  const colors = ["#d4f48d", "#f4b18d", "#bc90dd", "#99b8e8" ]
   // const getWeekdayString = (dateObj) => {
-  //   return dateObj.toLocaleDateString('en-CA', {weekday: 'short'}).substring(0, 3).toUpperCase();
-  // }
-
-  const getDateString = (dateObj) => {
-    return dateObj.toLocaleDateString('en-CA', {year: 'numeric', month: 'numeric', day: 'numeric'});
-  }
-  
-  
-  const WEEKDAYS = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+    //   return dateObj.toLocaleDateString('en-CA', {weekday: 'short'}).substring(0, 3).toUpperCase();
+    // }
+    
+    const getDateString = (dateObj) => {
+      return dateObj.toLocaleDateString('en-CA', {year: 'numeric', month: 'numeric', day: 'numeric'});
+    }
+    
+  const WEEKDAYS = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  const colors = ["#d4f48d", "#f4b18d", "#bc90dd", "#99b8e8" ];
+  const bufferSpace = 2;
   const context = useContext(UserContext);
   const [events, setEvents] = useState([]);
   const [weekdayIndex, setWeekdayIndex] = useState(new Date().getDay());
   const [focusDate, setFocusDate] = useState(getDateString(new Date()));
-  const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [earliestHour, setEarliestHour] = useState(24);
+  const [latestHour, setLatestHour] = useState(0);
 
-  const createEventsFromArray = (scheduleArray, user, colorIndex) => {
+  const createEventsFromArray = (scheduleArray, user, colorIndex, earliest, latest) => {
     let events = scheduleArray.filter(course => course.day_name.includes(WEEKDAYS[weekdayIndex]));
     events = events.map((course) => {
+      if (Number(course.time_start.slice(0, 2)) <= earliest.value)
+        earliest.value = (Math.max(0, Number(course.time_start.slice(0, 2)) - bufferSpace));
+      if (Number(course.time_end.slice(0, 2)) >= latest.value)
+        latest.value = (Math.min(24, Number(course.time_end.slice(0, 2)) + bufferSpace));
       return {
         start: `${focusDate} ${course.time_start}:00`,
         end: `${focusDate} ${course.time_end}:00`,
@@ -42,7 +46,9 @@ const CombinedScheduleView = ({navigation, route}) => {
 
   const createEvents = async () => {
     setLoading(true);
-    let eventsBuffer = createEventsFromArray(context.user.schedule, context.user, 0);
+    let latest = {value: latestHour};
+    let earliest = {value: earliestHour};
+    let eventsBuffer = createEventsFromArray(context.user.schedule, context.user, 0, earliest, latest);
     for (let i = 0; i < 3 && i < context.user.friend_list.length; i++) {
       try {
         const response = await fetch(`${BASE_URL}/${context.user.friend_list[i]}`, {
@@ -54,8 +60,10 @@ const CombinedScheduleView = ({navigation, route}) => {
         });
         const jsonResponse = await response.json();
         if (response.status === 200) {
-          let friendSchedule = createEventsFromArray(jsonResponse.schedule, jsonResponse, i+1);
+          let friendSchedule = createEventsFromArray(jsonResponse.schedule, jsonResponse, i+1, earliest, latest);
           friendSchedule.forEach((course) => eventsBuffer.push(course));
+          setEarliestHour(earliest.value);
+          setLatestHour(latest.value);
           setEvents(eventsBuffer);
         }
         else {
@@ -68,36 +76,17 @@ const CombinedScheduleView = ({navigation, route}) => {
     setLoading(false);
   }
 
-  // useFocusEffect(
-  //   React.useCallback(() => {
-  //     createEvents();
-  //     return () => {
-        
-  //     }
-  //   }, [])
-  // )
-
   useEffect(() => {
-    // let eventsBuffer = context.user.schedule.filter(course => course.day_name.includes(WEEKDAYS[weekdayIndex]));
-    // eventsBuffer = eventsBuffer.map((course) => {
-    //     return {
-    //       start: `${focusDate} ${course.time_start}:00`,
-    //       end: `${focusDate} ${course.time_end}:00`,
-    //       title: `${course.course_number} - ${course.course_name}`,
-    //       summary: `${context.user.username}`
-    //     }
-    // });
-    // setEvents(eventsBuffer);
     createEvents();
-
-  }, [context.user.schedule, context.user.friend_list, focusDate]);
+  }, [focusDate]);
   
-  // nav on tap
   // const _eventTapped = (event) => {
   //   console.log(event);
   // }
 
   const changeFocus = (dateString) => {
+    setLatestHour(0);
+    setEarliestHour(24);
     // means we went forward
     if (dateString > focusDate)
       setWeekdayIndex((weekdayIndex + 8) % 7);
@@ -120,6 +109,8 @@ const CombinedScheduleView = ({navigation, route}) => {
             dateChanged={changeFocus}
             scrollToFirst={true}
             size={2}
+            start={earliestHour}
+            end={latestHour}
           />
       }
     </View>
