99cefd48881220dded25602d5d5f6365d63323d4
Henry-Cevallos <henry.cevallos95@myhunter.cuny.edu>
2021-11-20 10:59:35 -0500

    moved get info out of callback

--- frontend/screens/CommonTimeText.js
+++ frontend/screens/CommonTimeText.js
@@ -128,6 +128,102 @@ const CommonTimeText = ({ navigation, route }) => {
         return schedule_times;
     }
 
+    //Does all async fetch calls to get free times of friends using algo
+    const getInfo = async() =>{
+        setLoading(true);
+        console.log("entered screen!");
+        try{
+            let response = await fetch(`${BASE_URL}/${context.user.id}`, {
+                method: 'GET', 
+                headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${context.user.token}`
+                },
+            });
+            response = await response.json();
+            
+            //We technically have our schedule from context now, but whatever
+            //we already have to make the call to get an updated friend list
+            let my_schedule = response.schedule;
+            let my_friends = response.friend_list;
+            
+            // Since a user's friend list can change based on other user's actions
+            // (such as accepting incoming requests or removing a friend),
+            // we'll take this as an opportunity to update our friend_list from
+            // the backend
+            if (context.user.friend_list !== my_friends) {
+                let userTemp = {...context.user};
+                userTemp.friend_list = my_friends;
+                context.setUser(userTemp)
+            }
+            
+            let curr_day = new Date().getDay();
+            const curr_hour = new Date().getHours();
+            const curr_min = new Date().getMinutes();
+            let curr_time = `${curr_hour}:${curr_min}`;
+
+            
+            curr_time = getTimeAsMin(curr_time);    //change curr time into an int 
+            curr_day = convertToDay(curr_day);  //change int into "MON" etc..
+
+            my_schedule = filterSchedule(my_schedule, curr_day); //filter classes for today only
+            let my_time_free = getMinutesOfSchedule(my_schedule, curr_time);
+
+            my_schedule = my_time_free;
+            my_time_free = getFreeTime(my_time_free);
+
+            
+            let friends = [];
+            for(const id of my_friends){
+                
+                try{
+                    response = await fetch(`${BASE_URL}/${id}`, {
+                        method: 'GET', 
+                        headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Token ${context.user.token}`
+                        },
+                    });
+                    response = await response.json();
+
+                } catch (error){
+                    console.error(error);
+                }
+
+                let friend_schedule = filterSchedule(response.schedule, curr_day);
+
+                if(friend_schedule.length <= 1){
+                    continue;
+                }
+                friend_schedule = getMinutesOfSchedule(friend_schedule);
+                friend_schedule = getFreeTime(friend_schedule, curr_time);
+
+                if(friend_schedule.length === 0){
+                    continue;
+                }
+
+                const now_bool = (curr_time >= friend_schedule[0][0]) && (curr_time < friend_schedule[0][1]);
+                console.log(now_bool);
+                friend_schedule = getCommonFreeTime(my_time_free, friend_schedule);
+                friend_schedule = convertToTime(friend_schedule);
+
+                const friend = {
+                    id: response.id,
+                    schedule: friend_schedule,
+                    f_name: response.first_name,
+                    l_name: response.last_name,
+                    now: now_bool
+                };
+
+                friends.push(friend);    
+            }
+            setItems(friends);
+            setLoading(false);
+        }catch(error){
+            console.error(error);
+        }
+    }
+
     // Because a user's friend_list can change outside of the user's control, such as if another friend
     // accepts their request, or someone defriends them, there can be times where the user's frontend
     // is behind what's on the backend. So even if we had useEffect listening to context.user.friend_list,
@@ -141,103 +237,7 @@ const CommonTimeText = ({ navigation, route }) => {
     // already in focused will not trigger it).
     useFocusEffect(
         React.useCallback(() => {
-            
-            const getInfo = async() =>{
-                setLoading(true);
-                console.log("entered screen!");
-                try{
-                    let response = await fetch(`${BASE_URL}/${context.user.id}`, {
-                        method: 'GET', 
-                        headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Token ${context.user.token}`
-                        },
-                    });
-                    response = await response.json();
-                    
-                    //We technically have our schedule from context now, but whatever
-                    //we already have to make the call to get an updated friend list
-                    let my_schedule = response.schedule;
-                    let my_friends = response.friend_list;
-                    
-                    // Since a user's friend list can change based on other user's actions
-                    // (such as accepting incoming requests or removing a friend),
-                    // we'll take this as an opportunity to update our friend_list from
-                    // the backend
-                    if (context.user.friend_list !== my_friends) {
-                        let userTemp = {...context.user};
-                        userTemp.friend_list = my_friends;
-                        context.setUser(userTemp)
-                    }
-                    
-                    let curr_day = new Date().getDay();
-                    const curr_hour = new Date().getHours();
-                    const curr_min = new Date().getMinutes();
-                    let curr_time = `${curr_hour}:${curr_min}`;
-    
-                    
-                    curr_time = getTimeAsMin(curr_time);    //change curr time into an int 
-                    curr_day = convertToDay(curr_day);  //change int into "MON" etc..
-    
-                    my_schedule = filterSchedule(my_schedule, curr_day); //filter classes for today only
-                    let my_time_free = getMinutesOfSchedule(my_schedule, curr_time);
-    
-                    my_schedule = my_time_free;
-                    my_time_free = getFreeTime(my_time_free);
-    
-                    
-                    let friends = [];
-                    for(const id of my_friends){
-                        
-                        try{
-                            response = await fetch(`${BASE_URL}/${id}`, {
-                                method: 'GET', 
-                                headers: {
-                                'Content-Type': 'application/json',
-                                'Authorization': `Token ${context.user.token}`
-                                },
-                            });
-                            response = await response.json();
-    
-                        } catch (error){
-                            console.error(error);
-                        }
-    
-                        let friend_schedule = filterSchedule(response.schedule, curr_day);
-    
-                        if(friend_schedule.length <= 1){
-                            continue;
-                        }
-                        friend_schedule = getMinutesOfSchedule(friend_schedule);
-                        friend_schedule = getFreeTime(friend_schedule, curr_time);
-
-                        if(friend_schedule.length === 0){
-                            continue;
-                        }
-
-                        const now_bool = (curr_time >= friend_schedule[0][0]) && (curr_time < friend_schedule[0][1]);
-                        console.log(now_bool);
-                        friend_schedule = getCommonFreeTime(my_time_free, friend_schedule);
-                        friend_schedule = convertToTime(friend_schedule);
-
-                        const friend = {
-                            id: response.id,
-                            schedule: friend_schedule,
-                            f_name: response.first_name,
-                            l_name: response.last_name,
-                            now: now_bool
-                        };
-        
-                        friends.push(friend);    
-                    }
-                    setItems(friends);
-                    setLoading(false);
-                }catch(error){
-                    console.error(error);
-                }
-            }
             getInfo();
-           
             // This is the cleanup function for useCallback.
             // It can return nothing, but is needed to run properly (I think)
             return () => {
