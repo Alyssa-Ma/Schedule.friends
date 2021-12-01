556cd7f9e9486bc65482809971b578e29f6507a1
Alyssa <Alyssa.Ma80@myhunter.cuny.edu>
2021-12-01 05:16:05 -0500

    sectioned out

--- frontend/components/SettingsComponent.js
+++ frontend/components/SettingsComponent.js
@@ -2,6 +2,7 @@ import React, {useState, useContext, useEffect} from 'react';
 import {View, SafeAreaView, StyleSheet, StatusBar, Image, TextInput, TouchableOpacity} from 'react-native';
 import UserContext from '../context/UserContext';
 import {Avatar, Title, Caption, Text, TouchableRipple, Switch } from 'react-native-paper';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const SettingsComponent = ({settingsOptions}) => {
     const context = useContext(UserContext);
@@ -11,11 +12,15 @@ const SettingsComponent = ({settingsOptions}) => {
     }
 
     return (
-        <View>
+        <ScrollView>
             {settingsOptions.map(({title, subtitle, onPress}, index) => (
               <TouchableOpacity key={title}>
                 <View>
                   <Text>{title}</Text>
+                  {subtitle && <Text>{subtitle}</Text>}
+                </View>
+                <View style={styles.section}>
+
                 </View>
               </TouchableOpacity>
             ))}
@@ -29,13 +34,17 @@ const SettingsComponent = ({settingsOptions}) => {
                 </View>
             </TouchableRipple>
             </View>
-        </View>
+        </ScrollView>
     );
 };
 
 export default SettingsComponent;
 
 const styles = StyleSheet.create({
+    section: {
+      height: 0.5,
+      backgroundColor: '#808080',
+    },
     container: {
       flex: 1,
     },
