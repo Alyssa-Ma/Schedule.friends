6ffbcce6e2e8c1a5943309b699d166aa326c5d64
Alyssa <Alyssa.Ma80@myhunter.cuny.edu>
2021-12-01 01:14:51 -0500

    added toggle for dark theme

--- frontend/screens/SettingsView.js
+++ frontend/screens/SettingsView.js
@@ -1,15 +1,25 @@
 import React, {useState, useContext, useEffect} from 'react';
 import {View, SafeAreaView, StyleSheet, StatusBar, Image, TextInput, TouchableOpacity} from 'react-native';
 import UserContext from '../context/UserContext';
-import {Avatar, Title, Caption, Text, TouchableRipple} from 'react-native-paper';
+import {Avatar, Title, Caption, Text, TouchableRipple, Switch } from 'react-native-paper';
 
 const SettingsView = ({ navigation }) => {
     const context = useContext(UserContext);
-    
+    const [isDarkTheme, setIsDarkTheme] = React.useState(false);
+    const toggleTheme = () => {
+        setIsDarkTheme(!isDarkTheme);
+    }
     return (
-      <Text>
-          Dark Theme
-      </Text>
+        <View style={styles.container}>
+            <TouchableRipple onPress={() => {toggleTheme()}}>
+                <View style={styles.listItem}>
+                    <Text>Dark Theme</Text>
+                    <View pointerEvents="none">
+                        <Switch value={isDarkTheme}></Switch>
+                    </View>
+                </View>
+            </TouchableRipple>
+        </View>
     );
   }
   
