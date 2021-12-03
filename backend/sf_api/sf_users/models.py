673cef6d5ff6f567e91de63a7c2fcc64e2634ec4
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2021-12-03 02:06:30 -0500

    added time validator for time_start and time_end

--- backend/sf_api/sf_users/models.py
+++ backend/sf_api/sf_users/models.py
@@ -3,16 +3,18 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 from django.contrib.postgres.fields import ArrayField
-
+from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
 
 def upload_to(instance, filename):
-    return 'profile_images/{id} - {filename}'.format(id=instance.id, filename=filename)
+    return 'profile_images/{id}/{filename}'.format(id=instance.id, filename=filename)
+
+time_validator = RegexValidator(r'^([0-1][0-9]|2[0-3]):[0-5][0-9]$', "Time must be [00-24]:[00-59] input")
 
 class User(AbstractUser):
     friend_list = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, default=None)
     friend_requests = models.ManyToManyField('FriendRequest', blank=True, default=None)
-    profile_image = models.ImageField(_("Image"), upload_to=upload_to, default='pimages/default.jpg')
+    profile_image = models.ImageField(_("Image"), upload_to=upload_to, default='profile_images/default.jpg')
     dark_mode = models.BooleanField(default=False)
 
     def __str__(self):
@@ -31,15 +33,12 @@ DAYS_OF_WEEK = [
 class Course(models.Model):
     owner = models.ForeignKey('User', related_name='schedule', on_delete=models.CASCADE, blank=True, null=True, default=None)
     day_name = ArrayField(models.CharField(max_length=3, choices=DAYS_OF_WEEK), size=7)
-    course_name = models.CharField(max_length=50)
+    course_name = models.CharField(max_length=60)
     course_number = models.CharField(max_length=30)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
-    
-    # time_start and time_end will likely need more approriate
-    # fields that will parse better with the front-end apps
-    time_start = models.CharField(max_length=7)
-    time_end = models.CharField(max_length=7)
+    time_start = models.CharField(max_length=5, validators=[time_validator])
+    time_end = models.CharField(max_length=5, validators=[time_validator])
 
     def __str__(self):
         return f"{self.course_number} - {self.course_name} for {self.owner}"
