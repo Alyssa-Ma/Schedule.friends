b0e42c501a9889d92931038b931c9f271c272655
Alyssa <Alyssa.Ma80@myhunter.cuny.edu>
2021-12-11 17:41:19 -0500

    changed to materialbottomtab for better look

--- frontend/components/FriendTabs.js
+++ frontend/components/FriendTabs.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import FriendRequestSend from '../screens/FriendRequestSend';
 import FriendsListStack from './FriendsListStack';
 import IncomingFriendRequestView from '../screens/IncomingFriendRequestView';
@@ -9,7 +9,7 @@ import { useTheme } from '@react-navigation/native';
 import UserContext from '../context/UserContext';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { color } from 'react-native-reanimated';
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 const size = 30;
 
 const FriendTabs = ({navigation, route}) => {
@@ -17,9 +17,6 @@ const FriendTabs = ({navigation, route}) => {
     const { toggleTheme } = useContext(UserContext);
     return (
         <Tab.Navigator initialRouteName="ViewFriends"
-            activeColor="#f0edf6"
-            inactiveColor="#3e2465"
-            barStyle={{ backgroundColor: '#694fad' }}
             screenOptions={{
                 tabBarHideOnKeyboard: true,
                 tabBarStyle: [
