cf0fcebd0799bcea4b649b3ebd79d8776fd2e9ea
Alyssa <Alyssa.Ma80@myhunter.cuny.edu>
2021-12-11 19:34:17 -0500

    added some faqs to the screen

--- frontend/screens/FaqView.js
+++ frontend/screens/FaqView.js
@@ -2,6 +2,7 @@ import React, {useState, useContext, useEffect} from 'react';
 import {View, SafeAreaView, StyleSheet, StatusBar, Image, TouchableOpacity, ScrollView} from 'react-native';
 import UserContext from '../context/UserContext';
 import { Title, Caption, Text, TouchableRipple, useTheme, Button } from 'react-native-paper';
+import { block } from 'react-native-reanimated';
 
 const FaqView = ({navigation, route}) => {
     const context = useContext(UserContext);
@@ -10,8 +11,44 @@ const FaqView = ({navigation, route}) => {
         <ScrollView style={{backgroundColor: colors.backgroundColor}}>
             <View style={styles.container}>
                 <Text style={styles.title}>FAQ</Text>
-                <Text style={styles.subtitle}>This would be where our FAQs would be...</Text>
-                <Text style={styles.tabText}>IF WE HAD ANY!!</Text>
+                <Text style={styles.subtitle}>Frequently Asked Questions</Text>
+                <Text style={styles.text}>
+                    Q: How do I update my profile after initial creation?
+                </Text>
+                <Text style={styles.tabText}>
+                    A: Go to 'My Profile' in the side drawer, and there will be a button to edit your profile there.
+                    It will have similar fields to the initial signup form.
+                </Text>
+                <Text style={styles.text}>
+                    Q: How do I choose which friends to show on my homepage?
+                </Text>
+                <Text style={styles.tabText}>
+                    A: Go to 'Home' in the side drawer. There will be a button to select friends at the bottom of the screen.
+                </Text>
+                <Text style={styles.text}>
+                    Q: How do I edit my class after I've already created it?
+                </Text>
+                <Text style={styles.tabText}>
+                    A: Tap on the class you want to edit. This will open the 'Edit Course' form where you can edit the details of that class.
+                </Text>
+                <Text style={styles.text}>
+                    Q: What does the 'Who's Free Now' tab do?
+                </Text>
+                <Text style={styles.tabText}>
+                    A: This tab will show you who is currently free on your friends list. If no one is currently free, it will not display anything.
+                </Text>
+                <Text style={styles.text}>
+                    Q: What does the 'Who's Free Now' tab do?
+                </Text>
+                <Text style={styles.tabText}>
+                    A: This tab will show you who is currently free on your friends list. If no one is currently free, it will not display anything.
+                </Text>
+                <Text style={styles.text}>
+                    Q: How do I logout?
+                </Text>
+                <Text style={styles.tabText}>
+                    A: You can logout in both the side drawer and in 'My Profile.'
+                </Text>
                 <TouchableOpacity onPress={() => navigation.pop()} style={[styles.button, {backgroundColor: colors.backgroundCardColors[2]}]}>
                     <Text style={styles.buttonText}>Go Back</Text>
                 </TouchableOpacity>
@@ -59,6 +96,7 @@ const styles = StyleSheet.create({
         marginLeft: 20,
         marginRight: 20,
         marginBottom: 10,
+        fontWeight: 'bold',
     },
     tabText: {
         textAlign: 'justify',
